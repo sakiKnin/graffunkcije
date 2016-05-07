37ab29c9e838f629a2a0a5e7cc56c438ab8a4a6c
ja.jajakovic <ja.jajakovic@gmail.com>
2016-05-07 20:41:09 +0200

    dfg

--- gulpfile.js
+++ gulpfile.js
@@ -26,9 +26,6 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
  *  main optimization build task
  */
 
-gulp.task('watch ui', watch);
-gulp.task('build ui', build);
-
 gulp.task('heroku', 'build');
 
 gulp.task('default', ['clean'], function () {
