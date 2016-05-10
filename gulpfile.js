526d8657441b66ade158a1090af25a4195749615
ja.jajakovic <ja.jajakovic@gmail.com>
2016-05-10 11:25:03 +0200

    izmjeni_cd

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
  *  main optimization build task
  */
 
-gulp.task('heroku', ['build']);
+gulp.task('heroku', 'build');
 
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
