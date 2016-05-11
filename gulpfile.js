51b006c82450a91e080a12a65872b4235edf610c
ja.jajakovic <ja.jajakovic@gmail.com>
2016-05-11 15:01:52 +0200

    x

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp')
+var sass = require('gulp-ruby-sass')
+var connect = require('gulp-connect')
+
+// requires browserify and vinyl-source-stream
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+
+gulp.task('connect', function () {
+    connect.server({
+        root: 'public',
+        port: 4000
+    })
+})
+
+gulp.task('browserify', function() {
+    // Grabs the app.js file
+    return browserify('./app/app.js')
+        // bundles it and creates a file called main.js
+        .bundle()
+        .pipe(source('main.js'))
+        // saves it the public/js/ directory
+        .pipe(gulp.dest('./public/js/'));
+})
+
+gulp.task('watch', function() {
+    gulp.watch('app/**/*.js', ['browserify'])
+})
+
+gulp.task('default', ['connect', 'watch'])
